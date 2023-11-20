b04e88ecb8a61e12609a3a9cb527c7e0b67d4c42
filipbojovic <fikakv@gmail.com>
2023-11-20 21:15:23 +0100

    Chore: Finished 05 (16, 17 and 18)

--- The-Complete-Guide-2023/05-states-and-events/src/components/Expenses/Expenses.js
+++ The-Complete-Guide-2023/05-states-and-events/src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card"
 import './Expenses.css'
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
+// this component controls expensesFilter component (its values)
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
